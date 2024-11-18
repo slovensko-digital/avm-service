029a47a5b9611c152a372190454e891b3bd873e9
Marek Ceľuch <celuchmarek@gmail.com>
2024-11-19 00:03:53 +0100

    Revert "fix body of deprectaed sign response (#12)"
    This reverts commit e03c6d0dfa0a95ac705028e45f0db3a7a7b9cd05.

--- service/src/main/java/digital/slovensko/autogram/service/dto/DeprecatedSignedDocumentResponse.java
+++ service/src/main/java/digital/slovensko/autogram/service/dto/DeprecatedSignedDocumentResponse.java
@@ -1,14 +1,25 @@
 package digital.slovensko.autogram.service.dto;
 
+import digital.slovensko.autogram.core.errors.UnrecognizedException;
 import digital.slovensko.autogram.core.server.dto.Document;
 import eu.europa.esig.dss.model.DSSDocument;
 
+import java.io.IOException;
+import java.util.Base64;
+
 public record DeprecatedSignedDocumentResponse(Document documentResponse, Signer signer) {
-    public static DeprecatedSignedDocumentResponse buildFormDSS(DSSDocument document, String signedBy, String issuedBy) {
-        return new DeprecatedSignedDocumentResponse(
-                (Document) Document.buildFromDSS(document),
-                new Signer(signedBy, issuedBy)
-        );
+        public static DeprecatedSignedDocumentResponse buildFormDSS(DSSDocument document, String signedBy, String issuedBy) {
+        try (var stream = document.openStream()) {
+            return new DeprecatedSignedDocumentResponse(
+                    new Document(
+                        Base64.getEncoder().encodeToString(stream.readAllBytes()),
+                        document.getMimeType().getMimeTypeString() + ";base64",
+                        document.getName()),
+                    new Signer(signedBy, issuedBy)
+            );
+        } catch (IOException e) {
+            throw new UnrecognizedException(e);
+        }
     }
 }
 
